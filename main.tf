25f0cea0c410583170262f28cd84ffc4dd955e80
Swapnil Shrishrimal <shrishrimalswapnil@gmail.com>
2023-08-25 13:53:51 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,15 +1,7 @@
 resource "aws_instance" "deployment" {
     ami = "ami-08a52ddb321b32a8c"
     instance_type = "t2.micro" // t2.micro, t2.small ..
-    user_data = <<EOF  
-#!/bin/bash
-sudo yum install -y yum-utils
-sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
-sudo yum install docker-ce docker-ce-cli containerd.io -y
-sudo usermod -aG docker ec2-user
-sudo systemctl start docker
-docker run -d -p 80:80 swapnil19/angular-app
-EOF
+    user_data = file("user_data.sh")
     tags = {
         Name = "Deployment Server"
     }
