0491e9f6d035d64833150e2fa200d78c76feac10
Swapnil Shrishrimal <shrishrimalswapnil@gmail.com>
2023-08-25 17:06:56 +0530

    Update user_data.sh

--- user_data.sh
+++ user_data.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-sudo yum install -y yum-utils
-sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
-sudo yum install docker-ce docker-ce-cli containerd.io -y
+sudo yum update -y
+sudo yum install docker -y
 sudo usermod -aG docker ec2-user
 sudo systemctl start docker
 docker run -d -p 80:80 swapnil19/angular-app
