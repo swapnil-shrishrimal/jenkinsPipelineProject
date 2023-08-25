29c19b38a6f1dd3c8ce14c60a9ab86ff449b2277
Swapnil Shrishrimal <shrishrimalswapnil@gmail.com>
2023-08-25 12:04:22 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "deployment" {
-    ami = "AMI_WHICH_YOU_WANT_TO_PROVISION"
-    instance_type = "TYPE_OF_INSTANCE" // t2.micro, t2.small ..
+    ami = "ami-08a52ddb321b32a8c"
+    instance_type = "t2.micro" // t2.micro, t2.small ..
     user_data = <<EOF  
 #!/bin/bash
 sudo yum install -y yum-utils
@@ -8,10 +8,10 @@ sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/dock
 sudo yum install docker-ce docker-ce-cli containerd.io -y
 sudo usermod -aG docker ec2-user
 sudo systemctl start docker
-docker run -d -p 80:80 swapnil19/angularapp
+docker run -d -p 80:80 swapnil19/angular-app
 EOF
     tags = {
         Name = "Deployment Server"
     }
-    vpc_security_group_ids = ["SECURITY_GROUP_TO_BE_GIVEN"]
+    vpc_security_group_ids = ["sg-045c84b3a0a181ac8"]
 }
