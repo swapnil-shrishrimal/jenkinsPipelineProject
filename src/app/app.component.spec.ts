c2e84502b30116a73f0a6b931ca37f09d28f7e15
Swapnil Shrishrimal <shrishrimalswapnil@gmail.com>
2023-08-28 00:06:16 +0530

    Update app.component.spec.ts

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -26,6 +26,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('angular-app app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('India becomes the first country to successfully land on the moon\'s south pole!');
   });
 });
